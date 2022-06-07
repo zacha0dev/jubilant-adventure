448d39dec39b53cf2698a4f5e3a7b8b1b5a0d04f
zacha0dev <93971118+zacha0dev@users.noreply.github.com>
2022-06-07 16:51:24 -0400

    Create rw_emails.py

---
+++ code-snippets/rw_emails.py
@@ -0,0 +1,37 @@
+import win32com.client
+import unicodecsv as csv
+import os
+
+output_file = open('./outlook_farming_001.csv','wb')
+output_writer = csv.writer(output_file, delimiter=';', encoding='latin2')
+
+outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+inbox = outlook.GetDefaultFolder(6).Folders.Item("Security Availabilities")
+messages = inbox.Items
+
+for i, message in enumerate(messages):
+    try:
+
+        sender = message.SenderName
+        sender_address = message.SenderEmailAddress
+        sent_to = message.To
+        date = message.LastModificationTime
+        subject = message.subject
+        body = message.body
+
+        attachments = message.Attachments
+        attachment = attachments.Item(1)
+        for attachment in message.Attachments:
+            attachment.SaveAsFile(os.path.join(output_file, str(attachment)))
+
+        output_writer.writerow([
+            sender,
+            sender_address,
+            subject,
+            body,
+            attachment])
+
+    except Exception as e:
+        ()
+
+output_file.close()
